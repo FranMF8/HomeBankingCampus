7cf5be444d86b18cd1e141fee023e11d72f9f669
FranMF8 <129801132+FranMF8@users.noreply.github.com>
2024-02-02 13:50:19 -0300

    Adds ClientsController
    Task 2 tested and completed

---
+++ HomeBankingMindHub/Controllers/ClientsController.cs
@@ -0,0 +1,100 @@
+﻿using HomeBankingMindHub.Models.DTOS;
+using HomeBankingMindHub.Models;
+using HomeBankingMindHub.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBankingMindHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private IClientRepository _clientRepository;
+
+        public ClientsController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var clients = _clientRepository.GetAllClients();
+
+                var clientsDTO = new List<ClientDTO>();
+
+                foreach (Client client in clients) { 
+
+                    var newClientDTO = new ClientDTO
+
+                    {
+                        Id = client.Id,
+                        Email = client.Email,
+                        FirstName = client.FirstName,
+                        LastName = client.LastName,
+
+                        Accounts = client.Accounts.Select(ac => new AccountDTO
+
+                        {
+                            Id = ac.Id,
+                            Balance = ac.Balance,
+                            CreationDate = ac.CreatedDate,
+                            Number = ac.Number
+
+                        }).ToList()
+                    };
+
+                    clientsDTO.Add(newClientDTO);
+
+                }
+
+                return Ok(clientsDTO);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            try
+            {
+                var client = _clientRepository.FindById(id);
+
+                if (client == null)
+                {
+                    return NotFound();
+                }
+
+                var clientDTO = new ClientDTO
+
+                {
+                    Id = client.Id,
+                    Email = client.Email,
+                    FirstName = client.FirstName,
+                    LastName = client.LastName,
+                    Accounts = client.Accounts.Select(ac => new AccountDTO
+                    {
+                        Id = ac.Id,
+                        Balance = ac.Balance,
+                        CreationDate = ac.CreatedDate,
+                        Number = ac.Number
+
+                    }).ToList()
+
+                };
+
+                return Ok(clientDTO);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
