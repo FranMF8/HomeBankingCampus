15c85047a64a0b8a44aea5952b8d4e5ac4b17782
FranMF8 <129801132+FranMF8@users.noreply.github.com>
2024-02-02 11:04:18 -0300

    Creates Account class

---
+++ HomeBankingMindHub/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeBankingMindHub.Models
+{
+    public class Account
+    {
+        public long Id { get; set; }
+        public string Number { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public double Balance { get; set; }
+
+        [ForeignKey("ClientId")]
+        public virtual Client Client { get; set; }
+    }
+}
