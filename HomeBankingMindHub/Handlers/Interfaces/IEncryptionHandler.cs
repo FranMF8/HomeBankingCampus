edd5e1b90f1d1d922bfc7fdb94c2e947b73beb75
FranMF8 <129801132+FranMF8@users.noreply.github.com>
2024-02-08 12:54:10 -0300

    Adds IEncryptionHandler

---
+++ HomeBankingMindHub/Handlers/Interfaces/IEncryptionHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace HomeBankingMindHub.Handlers.Interfaces
+{
+    public interface IEncryptionHandler
+    {
+        string EncryptPassword(string password, out byte[] hash, out byte[] salt);
+        bool ValidatePassword(string password, byte[] salt);
+    }
+}
