fc6f6af8a1a716516b6e42316f394c31f12a9684
FranMF8 <129801132+FranMF8@users.noreply.github.com>
2024-02-15 10:59:57 -0300

    adds type to TransactionsDTO

--- HomeBankingMindHub/DTOS/TransactionDTO.cs
+++ HomeBankingMindHub/DTOS/TransactionDTO.cs
@@ -5,6 +5,7 @@ namespace HomeBankingMindHub.DTOS
     public class TransactionDTO
     {
         public long Id { get; set; }
+        public string Type { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
