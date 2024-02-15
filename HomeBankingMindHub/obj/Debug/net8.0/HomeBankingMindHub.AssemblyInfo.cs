bef48610dc03164603c46e084df97afb3b22eaf8
FranMF8 <129801132+FranMF8@users.noreply.github.com>
2024-02-15 11:02:48 -0300

    Adds type to TransactionDTO

--- HomeBankingMindHub/obj/Debug/net8.0/HomeBankingMindHub.AssemblyInfo.cs
+++ HomeBankingMindHub/obj/Debug/net8.0/HomeBankingMindHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeBankingMindHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b2c484f2f53d97cee757fa3d57500900bf2d51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeBankingMindHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeBankingMindHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
