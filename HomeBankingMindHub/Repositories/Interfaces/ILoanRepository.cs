f104f57eafeded4ef10908abf52286b042b1fe25
FranMF8 <129801132+FranMF8@users.noreply.github.com>
2024-02-16 08:10:32 -0300

    Adds LoanDTO and LoanApplicationDTO

---
+++ HomeBankingMindHub/Repositories/Interfaces/ILoanRepository.cs
@@ -0,0 +1,13 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Repositories.Interfaces
+{
+    public interface ILoanRepository
+    {
+        IEnumerable<Loan> GetAllCards();
+        void Save(Loan loan);
+        Loan FindById(long id);
+        Loan FindByNumber(string number);
+        IEnumerable<Loan> GetLoansByClient(long clientId);
+    }
+}
