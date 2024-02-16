8e338fe97227bc5f17260bd31e56772536f94b6d
FranMF8 <129801132+FranMF8@users.noreply.github.com>
2024-02-16 08:00:30 -0300

    Changes .gitignore

--- HomeBankingMindHub/obj/Debug/net8.0/HomeBankingMindHub.AssemblyInfo.cs
+++ HomeBankingMindHub/obj/Debug/net8.0/HomeBankingMindHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeBankingMindHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef48610dc03164603c46e084df97afb3b22eaf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeBankingMindHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeBankingMindHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
